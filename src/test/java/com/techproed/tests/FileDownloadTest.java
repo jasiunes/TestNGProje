538a47fbd30c8b3a9cf1dd7ee6b26d2cbb4f70a2
yasemin <muazunes@gmail.com>
2020-09-08 22:16:49 +0200

    Hotel

--- src/test/java/com/techproed/tests/FileDownloadTest.java
+++ src/test/java/com/techproed/tests/FileDownloadTest.java
@@ -1,50 +1,46 @@
 package com.techproed.tests;
-
 import com.techproed.utilities.TestBase;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebElement;
 import org.testng.Assert;
 import org.testng.annotations.Test;
-
 import java.nio.file.Files;
 import java.nio.file.Paths;
-
 public class FileDownloadTest extends TestBase {
     @Test
-    public void dosyaVarMi() {
+    public void dosyaVarMi(){
         System.out.println(System.getProperty("user.dir"));
         System.out.println(System.getProperty("user.home"));
-
-          // C:\Users\Muaz\IdeaProjects\TestNGProje
-        //  C:\Users\Muaz
-        String kullaniciDosyaYolu= System.getProperty("user.dir");
-        String pomXmlDosyaYolu= kullaniciDosyaYolu + "/pom.xml";
+        String kullaniciDosyaYolu = System.getProperty("user.dir");
+        // C:/Users/isimsiz/IdeaProjects/TestNGProje/pom.xml
+        String pomXmlDosyaYolu = kullaniciDosyaYolu +"/pom.xml";
         boolean varMi = Files.exists(Paths.get(pomXmlDosyaYolu));
         Assert.assertTrue(varMi);
-        System.out.println(varMi);
     }
     @Test
-    public void dosyaUpload(){
-        //bilgisayardan websayfasina dosya yükleme
+    public void dosyaUpload(){ // bilgisayardan websayfasına doysa yükleme
         driver.get("http://the-internet.herokuapp.com/upload");
-        // C:\Users\Muaz\Desktop\Techpro
-        //<input id="file-upload" type="file" name="file">
         WebElement dosyaSecmeButonu = driver.findElement(By.id("file-upload"));
-        //yüklemek istedigimiz dosyanin ,path(dosya yolunu) ekleyelim.
-        dosyaSecmeButonu.sendKeys(" C:\\Users\\Muaz\\Desktop\\Techpro\\logo.png");
+        // yüklemek istediğimiz dosyanın, path(dosya yolunu) ekleyelim.
+        dosyaSecmeButonu.sendKeys("C:/Users/isimsiz/upload/logo.png");
+        //dosyaSecmeButonu.sendKeys("C:\\Users\\isimsiz\\upload\\logo.png");
         WebElement uploadButonu = driver.findElement(By.id("file-submit"));
         uploadButonu.click();
-         WebElement fileUploadedYazisi = driver.findElement(By.tagName("h3"));
+        WebElement fileUploadedYazisi = driver.findElement(By.tagName("h3"));
         Assert.assertTrue(fileUploadedYazisi.isDisplayed());
     }
     @Test
     public void dosyaDownload(){
-        WebElement  amsterdamLinki = driver.findElement(By.partialLinkText("Amsterdam.jpg"));
+        // C:\Users\isimsiz\Downloads\Amsterdam.jpg
+        driver.get("http://the-internet.herokuapp.com/download");
+        WebElement amsterdamLinki = driver.findElement(By.partialLinkText("Amsterdam.jpg"));
         amsterdamLinki.click();
-//<a href="download/Amsterdam.jpg">Amsterdam.jpg</a>
-
-        boolean varMi =Files.exists(Paths.get("C:\\Users\\Muaz\\Downloads"));
+        try {
+            Thread.sleep(3000);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+        boolean varMi = Files.exists(Paths.get("C:\\Users\\isimsiz\\Downloads\\Amsterdam.jpg"));
         Assert.assertTrue(varMi);
-
     }
 }
